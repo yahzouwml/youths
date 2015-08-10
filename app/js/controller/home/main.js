8c7684f943331e987d9f8f5322f6dd0a0134507c
mcgradysun <mcgrady.sun@btcchina.com>
2015-08-10 16:54:54 +0800

    complete login,logout

--- app/js/controller/home/main.js
+++ app/js/controller/home/main.js
@@ -1,3 +1,48 @@
-app.controller('mainCtrl',['$rootScope','$scope',function($rootScope,$scope){
+app.controller('mainCtrl', ['$rootScope', '$scope', 'ipCookie', 'AuthService', function($rootScope, $scope, ipCookie, AuthService) {
+    if ($rootScope.currentUser == null) {
+        console.log(ipCookie('currentUser'))
+        $rootScope.currentUser = ipCookie('currentUser')
+    }
 
-}]);
+
+    $scope.register = function() {
+        AuthService.register($scope.user.email, $scope.user.password).then(function(response) {
+            console.log(response);
+            if (response.$resolved) {
+                $scope.notify('success', "注册成功");
+                $scope.user = {};
+                $scope.form.$setPristine();
+            } else {
+                $scope.notify('error', "注册失败");
+            }
+        });
+    }
+
+    $scope.login = function() {
+        AuthService.login($scope.user.email, $scope.user.password).then(function(response) {
+            console.log(response);
+            if (response.$resolved) {
+                $scope.notify('success', "登录成功");
+                $scope.user = {};
+                $scope.form.$setPristine();
+            } else {
+                $scope.notify('error', "登录失败");
+            }
+            $rootScope.currentUser = {
+                id: response.user.id,
+                tokenId: response.id,
+                email: response.user.email
+            }
+            ipCookie('currentUser', $rootScope.currentUser, {
+                expires: 7
+            });
+        })
+    }
+
+    $scope.logout = function() {
+        AuthService.logout().then(function(response) {
+            $rootScope.currentUser = null
+            ipCookie.remove('currentUser')
+        })
+    }
+}])
--- app/js/services/auth.js
+++ app/js/services/auth.js
@@ -4,22 +4,12 @@ app.factory('AuthService', ['User','$q', '$rootScope', function(User, $q,
       return User
         .login({email: email, password: password})
         .$promise
-        .then(function(response) {
-          $rootScope.currentUser = {
-            id: response.user.id,
-            tokenId: response.id,
-            email: email
-          };
-        });
     }
 
     function logout() {
       return User
        .logout()
        .$promise
-       .then(function() {
-         $rootScope.currentUser = null;
-       });
     }
 
     function register(email, password) {
