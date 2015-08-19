2427f4490413ca6808fb59118fec2995c5499583
mcgradysun <mcgrady.sun@btcchina.com>
2015-08-19 18:19:25 +0800

    edit personal center

--- app/js/controller/account/center.js
+++ app/js/controller/account/center.js
@@ -1,3 +1,10 @@
 app.controller('accountCenterCtrl', ['$rootScope', '$scope', '$q', 'User', function($rootScope, $scope, $q, User) {
-
+    $scope.getUser = function() {
+        User.findById({
+            id: $rootScope.currentUser.id
+        }).$promise.then(function(response) {
+            console.log(response)
+            $scope.User = response
+        })
+    }
 }])
