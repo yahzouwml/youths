a97e79703c0163bd4e4c1224139648911ceecb1e
mcgradysun <mcgrady.sun@btcchina.com>
2015-09-01 18:24:30 +0800

    edit blog

--- app/js/filter/common.js
+++ app/js/filter/common.js
@@ -1,5 +1,16 @@
-app.filter('htmlToPlaintext', function() {
-    return function(text,length) {
-        return text ? String(text).replace(/<[^>]+>/gm, '').substr(0,length) : '';
-    };
-})
+app
+    .filter('htmlToPlaintext', function() {
+        return function(text, length) {
+            return text ? String(text).replace(/<[^>]+>/gm, '').substr(0, length) : '';
+        };
+    })
+    .filter('thumb', function() {
+        return function(text) {
+            var imgSrc = ''
+            text.replace(/<img [^>]*src=['"]([^'"]+)[^>]*>/gi, function(match, capture) {
+                imgSrc = capture;
+            })
+
+            return imgSrc;
+        };
+    })
