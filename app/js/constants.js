70e8d48d145a341d41ae710537082073f5aeda5a
TNT-Likely <532837139@qq.com>
2015-09-07 00:14:47 +0800

    delete not sync file

--- app/js/constants.js
+++
@@ -1,9 +0,0 @@
-angular.module("envconfig", [])
-
-.constant("ENV", {
-	"mainServer": "http://localhost:7000/",
-	"storageServer": "http://localhost:3010/",
-	"apiServer": "http://localhost:3010/"
-})
-
-;
