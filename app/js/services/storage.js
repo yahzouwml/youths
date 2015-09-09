d1dd4900b14b826f00b9f4f06d9b21939bf4b5c4
mcgradysun <mcgrady.sun@btcchina.com>
2015-09-09 14:33:46 +0800

    storage service

---
+++ app/js/services/storage.js
@@ -0,0 +1,34 @@
+app.service('StorageService', ['User', '$q', '$http', '$rootScope', function(User, $q, $http, $rootScope) {
+    this.dataURItoBlob = function(dataURI) {
+        // convert base64/URLEncoded data component to raw binary data held in a string
+        var byteString;
+        if (dataURI.split(',')[0].indexOf('base64') >= 0)
+            byteString = atob(dataURI.split(',')[1]);
+        else
+            byteString = unescape(dataURI.split(',')[1]);
+
+        // separate out the mime component
+        var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+        // write the bytes of the string to a typed array
+        var ia = new Uint8Array(byteString.length);
+        for (var i = 0; i < byteString.length; i++) {
+            ia[i] = byteString.charCodeAt(i);
+        }
+
+        return new Blob([ia], {
+            type: mimeString
+        });
+    }
+
+    this.upload = function(dataURI, containerName, fileName) {
+        var fd = new FormData();
+        fd.append('file', this.dataURItoBlob(dataURI), fileName)
+        return $http.post($rootScope.apiServer + '/containers/' + containerName + '/upload', fd, {
+            transformRequest: angular.identity,
+            headers: {
+                'Content-Type': undefined
+            }
+        })
+    }
+}]);
