e6c26c816b920f71b31c179a8773767cce97d658
mcgradysun <mcgrady.sun@btcchina.com>
2015-10-10 18:16:24 +0800

    edit zhihu.user

--- app/js/filter/storage.js
+++ app/js/filter/storage.js
@@ -3,4 +3,10 @@ app
         return function(fileName, containerName) {
             return $rootScope.apiServer + '/containers/' + containerName + '/download/' + fileName
         }
+    }])
+    .filter('zhihuavatar', ['$rootScope', function($rootScope) {
+        return function(fileName) {
+            console.log(fileName);
+            return $rootScope.apiServer + '/containers/zhihuuser/download/' + fileName.split('/')[2] + "-" + fileName.split('/')[3]
+        }
     }]);
