e51a127d69c8fe364a39529dc876124856548f13
likely <xiaoxiao.sun@dianrong.com>
2016-01-12 18:35:19 +0800

    improvement : beautiful ui

--- app/js/ui/ui.js
+++ app/js/ui/ui.js
@@ -1,30 +1,30 @@
 $(document).ready(function() {
-    var bg = function() {
-        $('.banner').height($(window).height())
-        $('.b-search').css({
-            paddingTop: ($(window).height() - 220) / 2
-        })
-    }
-
-    $(document).on('click', '.tags2 a', function() {
-        $('#blog .active').removeClass('active')
-        $(this).addClass('active')
+  var bg = function() {
+    $('.banner').height($(window).height())
+    $('.b-search').css({
+      paddingTop: ($(window).height() - 140) / 2
     })
+  }
 
+  $(document).on('click', '.tags2 a', function() {
+    $('#blog .active').removeClass('active')
+    $(this).addClass('active')
+  })
+
+  bg()
+  $(window).bind('resize', function() {
     bg()
-    $(window).bind('resize', function() {
-        bg()
-    })
+  })
 
-    $(document).on('mouseover', '.u-avatar', function() {
-        $(this).siblings('.u-extend').fadeIn(500);
-    });
+  $(document).on('mouseover', '.u-avatar', function() {
+    $(this).siblings('.u-extend').fadeIn(500);
+  });
 
-    $(document).on('mouseout', '.u-avatar', function() {
-        $(this).siblings('.u-extend').fadeOut(100);
-    });
+  $(document).on('mouseout', '.u-avatar', function() {
+    $(this).siblings('.u-extend').fadeOut(100);
+  });
 
-    $(document).on('mouseover', '[title!=""]', function() {
-        $(this).tooltip('show');
-    });
+  $(document).on('mouseover', '[title!=""]', function() {
+    $(this).tooltip('show');
+  });
 });
